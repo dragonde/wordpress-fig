5e223d60de92ff8ed23b11ce2dca646a23d27f5b
Dragonde <dragonde.es@gmail.com>
2014-11-28 21:11:58 +0100

    nginx-plugin

---
+++ download-nginx-plugin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+  # Download nginx helper plugin
+  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
+  unzip -o nginx-helper.*.zip -d wordpress/wp-content/plugins
+
