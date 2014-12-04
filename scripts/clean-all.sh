618a8d240aaaa65f2d30165704f0ffa56fdeb9c6
Alberto B <dragonde.es@gmail.com>
2014-12-04 17:06:23 +0100

    myrootdir con slash

--- scripts/clean-all.sh
+++ scripts/clean-all.sh
@@ -7,5 +7,5 @@ rm dbinit/*.sql
 
 perl -pi -e "s/^(\s+MYSQL_ROOT_PASSWORD:).*$/\1 mypassword/" fig.yml
 perl -pi -e "s/^(\s+WORDPRESS_PASSWORD:).*$/\1 mypassword/" fig.yml
-perl -pi -e "s/^(\s+-\s*)\/.+(\/.+:)\s*(\/.+)\s*$/\1myrootdirectory\2\3\n/" fig.yml
+perl -pi -e "s/^(\s+-\s*\/).+(\/.+:)\s*(\/.+)\s*$/\1myrootdirectory\2\3\n/" fig.yml
 
--- scripts/configure-fig.sh
+++ scripts/configure-fig.sh
@@ -8,6 +8,6 @@ cwd=$(pwd)
 perl -pi -e "s/^(\s*MYSQL_ROOT_PASSWORD:).*$/\1 $mysql_pw/" fig.yml
 perl -pi -e "s/^(\s*WORDPRESS_PASSWORD:).*$/\1 $wordpress_pw/" fig.yml
 #Cuidado la variable cwd contiene /
-perl -pi -e "s#myrootdirectory#$cwd#" fig.yml
+perl -pi -e "s#/myrootdirectory#$cwd#" fig.yml
 
 
