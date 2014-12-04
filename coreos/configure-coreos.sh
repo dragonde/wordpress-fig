764acc3a9afaa760e72ad670f096545c9918730c
Alberto B <dragonde.es@gmail.com>
2014-12-04 22:40:25 +0100

    ruta a coreos/configure-coreos.sh

--- coreos/configure-coreos.sh
+++ coreos/configure-coreos.sh
@@ -4,7 +4,7 @@ mysql_pw=$(openssl rand -base64 32 | head -c 40 | tr '/+' '01')
 wordpress_pw=$(openssl rand -base64 32 | head -c 40 | tr '/+' '01')
 cwd=$(pwd)
 
-sed -i -re "s/mypassword1/$mysql_pw/" db.1.service
-sed -i -re "s/mypassword2/$wordpress_pw/" web.1.service
-sed -i -re "s#/myrootdirectory#$cwd#g" web.1.service
+sed -i -re "s/mypassword1/$mysql_pw/" coreos/db.1.service
+sed -i -re "s/mypassword2/$wordpress_pw/" coreos/web.1.service
+sed -i -re "s#/myrootdirectory#$cwd#g" coreos/web.1.service
 
